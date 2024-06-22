6d2cbacf6e29fb4e2683042f83edeb8bb821ad69
SteamnGFX <angelroberto7777@gmail.com>
2024-06-22 09:13:21 -0600

    Se agregó un componente compartido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AcumuladorComponent } from './acumulador/acumulador.component';
 import { ClientesModule } from './clientes/clientes.module';
+import { CompartidoModule } from './compartido/compartido.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AcumuladorComponent,ClientesModule],
+  imports: [RouterOutlet,AcumuladorComponent,ClientesModule,CompartidoModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/compartido/compartido.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+
+
+
+@NgModule({
+  declarations: [
+    CabeceraComponent
+  ],
+  imports: [
+    CommonModule
+  ], exports:[
+    CabeceraComponent
+  ]
+})
+export class CompartidoModule { }
